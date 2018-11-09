8ea704e8d777f828edab81cb5ae9d109a48eb121
prateekmurmu <42237322+prateekmurmu@users.noreply.github.com>
2018-11-09 23:55:02 +0530

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -1,9 +1,9 @@
 //a program to declare two variables a,b and a pointer variable p (all integer type).Try to understand pointers while doing this.Point p to a. And store the value pointed by p in b. Print the values of a, b and *p.Assign values a=2 and b = 3. Print the values of a, b and *p. 
 //Now point p to b. Print the values of a, b and *p.
-
+//include the library
 #include<iostream>
 using namespace std;
-
+//start the main function
 int main()
 {
     int a;
